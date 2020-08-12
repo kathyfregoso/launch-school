767e06ce4628ca1eff7b249dce7896088ae074ea
kathyfregoso <kathyfregoso@gmail.com>
2020-08-11 17:48:56 -0700

    Update capitalize.js

--- JS100/07-Strings/capitalize.js
+++ JS100/07-Strings/capitalize.js
@@ -2,9 +2,3 @@ let str = 'launch school tech & talk';
 let newStr = str.split(' ').map((el) => el[0].toUpperCase() + el.slice(1)).join(" "); // also works with .substring() method
 
 console.log(newStr);
-
-
-
-
-
-
